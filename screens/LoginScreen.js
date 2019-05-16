b452b592136d74e656c92b8be02c7214ee74c1c2
Ella Wirstad <ella.wg@hotmail.com>
2019-05-16 16:32:35 +0200

    add button component

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, Button, View } from 'react-native';
+import ButtonComponent from '../components/ButtonComponent';
 
 export default class LoginScreen extends React.Component {
   render() {
     return (
       <View>
         <Text style={styles.loginText}>Hej Looogiiin</Text>
-        <Button title="Go to map" onPress={() => this.props.navigation.navigate('map')} />
+        <Button title="Go to map" />
+        <ButtonComponent buttonTitle="Login" />
       </View>
     );
   }
