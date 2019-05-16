3b3258eda0ed7e7428eb9be8bf2a99dcb62c9bbf
JOhohannes Loor <johannes.loor@gmail.com>
2019-05-16 15:04:16 +0200

    Changed to AppNavigator in app

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <MapScreen />
+          <AppNavigator />
         </View>
       );
     }
