7078a74c4a0e13405fe16d691858166a7dd3bb21
JOhohannes Loor <johannes.loor@gmail.com>
2019-05-19 17:44:39 +0200

    Fixed stylesheet on marker

--- components/Marker.js
+++ components/Marker.js
@@ -12,27 +12,19 @@ export class CustomMarker extends Component {
           width={200}
           popover={
             <TouchableOpacity
-              style={{
-                flex: 1,
-                height: 180,
-                width: 180,
-              }}
+              style={styles.popUp}
               onPress={() => {
                 alert('Du skickas nu vidare till nästa skärm');
                 {
                   /*this.props.navigation.navigate('challenge + {this.props.id}')*/
                 }
               }}>
-              <Text style={{ color: 'white', fontSize: 20 }}>{this.props.title}</Text>
-              <Image
-                style={{ flex: 1, height: null, width: null, resizeMode: 'contain' }}
-                source={this.props.img}
-              />
+              <Text style={[styles.textColor, styles.titleSize]}>{this.props.title}</Text>
+              <Image style={styles.img} source={this.props.img} />
 
-              <Text style={{ color: 'white' }}>{this.props.description}</Text>
+              <Text style={styles.textColor}>{this.props.description}</Text>
             </TouchableOpacity>
-          }
-          backgroundColor="black">
+          }>
           <Icon reverse name={this.props.icon} type="font-awesome" color="black" />
         </Tooltip>
       </MapView.Marker>
@@ -40,4 +32,23 @@ export class CustomMarker extends Component {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textColor: {
+    color: 'white',
+  },
+  titleSize: {
+    fontSize: 20,
+  },
+  img: {
+    flex: 1,
+    height: null,
+    width: null,
+    resizeMode: 'contain',
+  },
+  popUp: {
+    flex: 1,
+    height: 180,
+    width: 180,
+    backgroundColor: 'black',
+  },
+});
