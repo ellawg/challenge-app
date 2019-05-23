b286350bb5c239b6bc4bbc773c529596fcd06cca
lindacnatt <linda.cnattingius@gmail.com>
2019-05-23 13:43:13 +0200

    remove en sak jag inte använde

--- screens/CreateChallengeScreen.js
+++ screens/CreateChallengeScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { Input, ButtonGroup, Button } from 'react-native-elements';
 import CustomModal from '../components/Modal.js';
