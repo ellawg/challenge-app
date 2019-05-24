58bdb8c5f7aa7241281d5459cbb12408f38cf659
JOhohannes Loor <johannes.loor@gmail.com>
2019-05-24 17:28:55 +0200

    Removed dead code

--- components/Marker.js
+++ components/Marker.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
-import { Button, ThemeProvider } from 'react-native-elements';
 import MapView, { Callout } from 'react-native-maps';
 
 /* Must be used inside <Mapview> to create marker
